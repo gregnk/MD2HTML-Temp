da1c252d68396bec4349a32ae442533a121152bd
Gregory Karastergios <gregory@gregnk.com>
2019-04-21 11:21:33 -0400

    Updated assembly info

--- src/Md2Html/Properties/AssemblyInfo.cs
+++ src/Md2Html/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MD2HTML")]
+[assembly: AssemblyTitle("Md2Html")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MD2HTML")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Md2Html")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
